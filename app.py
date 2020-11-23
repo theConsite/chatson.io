d67849ffcb34827e28a9c4ac2fbaf4f36e4baf10
Marcel <marcel.kaminski@ucdconnect.ie>
2020-11-23 19:23:19 +0100

    working version v1

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify
+from cryptography.fernet import Fernet
 import sqlite3
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+message = 'John Doe'
+file = open("key.key", "rb")
+key_main = file.read()
+file.close()
+
+
+def encrypt(message, key: bytes) -> bytes:
+    message = message.encode()
+    return Fernet(key).encrypt(message)
+
+
+def decrypt(token, key: bytes) -> bytes:
+    return Fernet(key).decrypt(token).decode()
 
 
 def send_mess(user_number, chat, key, mess_counter_current):
     conn = sqlite3.connect('sqlite.db')
     c = conn.cursor()
     mess_counter_current = str(int(mess_counter_current)+1)
-    to_execute = (user_number, chat, key, mess_counter_current)
+    to_execute = (encrypt(user_number, key_main), encrypt(chat, key_main),
+                  key, encrypt(mess_counter_current, key_main))
     c.execute(
         "INSERT INTO chat_table ( user_number,   chat,  [key],  mess_counter    )  VALUES (?, ?, ?,?    );", to_execute)
     conn.commit()
@@ -33,7 +48,8 @@ def get_mess(user_number, key, mess_counter_current):
 
 def prepare_mess(messeges):
     for i in messeges:
-        dic_to_add = {"user": list(i)[:-2][0], "mess": list(i)[:-2][1]}
+        dic_to_add = {"user": decrypt(
+            list(i)[:-2][0], key_main), "mess": decrypt(list(i)[:-2][1], key_main)}
         session["messgeges"].append(dic_to_add)
 
 
@@ -46,7 +62,8 @@ def index():
         session["messgeges"] = []
         return redirect("/chat", code=302)
     return render_template("index.html")
-        
+
+
 @app.route('/enter', methods=['GET', 'POST'])
 def init():
     if request.method == "POST" and request.form.get("key_id") != "" and request.form.get("user") != None:
@@ -60,28 +77,25 @@ def init():
 
 @app.route('/chat', methods=["GET", 'POST'])
 def sessions():
-    try:
-        print(session["key"], session["user"])
-        # if session["key"] != None and session["user"] != None:
+    print(session["key"], session["user"])
+    # if session["key"] != None and session["user"] != None:
 
-        if request.method == "POST":
-            print("Post")
-            chat_mess = request.form.get('chat_mess')
-            if chat_mess == "":
-                return render_template('chat.html', error="You can't send blank mess!")
-            else:
-                # send mess
-                send_mess(session["user"], chat_mess, session["key"], str(
-                    session["mess_counter_current"]))
-                # messeges=get_mess(session["user"],session["key"],str(session["mess_counter_current"]))
-                session["mess_counter_current"] += 1
-                # prepare_mess(messeges)
+    if request.method == "POST":
+        print("Post")
+        chat_mess = request.form.get('chat_mess')
+        if chat_mess == "":
+            return render_template('chat.html', error="You can't send blank mess!")
+        else:
+            # send mess
+            send_mess(session["user"], chat_mess, session["key"], str(
+                session["mess_counter_current"]))
+            # messeges=get_mess(session["user"],session["key"],str(session["mess_counter_current"]))
+            session["mess_counter_current"] += 1
+            # prepare_mess(messeges)
 
-        return render_template('chat.html')
-        # else:
-        #     return render_template('enter.html')
-    except:
-        return redirect("/enter", code=302)
+    return render_template('chat.html')
+    # else:
+    #     return render_template('enter.html')
 
 
 @app.route('/chat_check', methods=['GET', 'POST'])
@@ -91,7 +105,8 @@ def check_chat():
     prepare_mess(messeges)
     return jsonify(session["messgeges"])
 
+
 @app.route('/chat_new', methods=['GET', 'POST'])
 def chat_new():
-    
+
     return render_template('chat_new.html')
